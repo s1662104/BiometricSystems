d33b80f36a3167ace8bc5f767a5c49db87c3e4ea
manuel <manuel.bianchi1@gmail.com>
2021-02-18 18:19:57 +0100

    Antispoofing temporaneo nel mio branch nuovo

--- Database.py
+++ Database.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -123,3 +124,53 @@ if __name__ == '__main__':
     gallery_data,gallery_target,probe_data,probe_target = db.split_data()
     print("gallery:", len(gallery_data), len(gallery_target), len(np.unique(gallery_target)))
     print("probe:", len(probe_data), len(probe_target), len(np.unique(probe_target)))
+=======
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import tarfile
+import cv2
+import os
+
+class Database():
+    def __init__(self):
+        # self.data=np.load("Olivetti_faces/olivetti_faces.npy")
+        # self.target=np.load("Olivetti_faces/olivetti_faces_target.npy")
+
+        self.data = []
+        self.target = []
+        tar = tarfile.open("LFW\lfw-funneled.tgz", "r:gz")
+        counter = 0
+        for tarinfo in tar:
+
+            tar.extract(tarinfo.name)
+            if tarinfo.name[-4:] == ".jpg":
+                image = cv2.imread(tarinfo.name, cv2.IMREAD_COLOR)
+                image = cv2.resize(image, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_AREA)
+                self.data.append(np.array(image))
+                counter += 1
+
+                name = tarinfo.name.split("/")[1]
+                self.target.append(name)
+            if tarinfo.isdir():
+                pass
+            else:
+                os.remove(tarinfo.name)
+        tar.close()
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    db = Database()
+    print("Numero utenti: ",len(np.unique(db.target)))
+
+    print("Nome utente: ",db.target[1])
+
+    plt.imshow(db.data[1])
+    plt.show()
+>>>>>>> c30ed1e... Antispoofing eye blink, da agganciarlo al main vero e proprio rimuovendo quello locale
--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:Main.py
 import dlib
 import numpy as np
 import cv2
@@ -48,4 +49,58 @@ if __name__ == '__main__':
 
         # When everything done, release the capture
         cap.release()
+=======
+import dlib
+import numpy as np
+import cv2
+
+message = "Inserire codice fiscale: "
+error = "Codice fiscale non valido"
+dim_image = 64
+
+def detect_face(img, vis):
+    crop = None
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+    dets = detector(img, 1)  # Detect the faces in the image
+    for i, d in enumerate(dets):
+        landmark = predictor(img, d)
+        top = landmark.part(19).y
+        left = landmark.part(0).x
+        right = landmark.part(16).x
+        bottom = landmark.part(8).y
+        crop = img[top:bottom, left:right]
+        cv2.rectangle(vis, (left, top), (right, bottom), (0, 255, 0), 3)
+        crop = cv2.resize(crop, (dim_image, dim_image))
+    cv2.imshow('Face', crop)
+    #return crop
+
+
+if __name__ == '__main__':
+    cf = input(message)
+    if (len(cf)!=16): print(error)
+    else:
+        cap = cv2.VideoCapture(2)
+        # Check if camera opened successfully
+        # if (cap.isOpened() == False):
+        #     print("Error opening video stream or file")
+        while (True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+
+            vis = frame.copy()
+
+            # Our operations on the frame come here
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            detect_face(gray, vis)
+
+            # Display the resulting frame
+            cv2.imshow('frame', vis)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # When everything done, release the capture
+        cap.release()
+>>>>>>> c30ed1e... Antispoofing eye blink, da agganciarlo al main vero e proprio rimuovendo quello locale:FaceRecognition.py
         cv2.destroyAllWindows()
