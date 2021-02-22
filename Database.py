c29a0151aa1ddaf1fda4a73ac43c85aadb45ab70
ile_ <staiti.1662105@studenti.uniroma1.it>
2021-02-22 17:14:44 +0100

    suddivisione train e test set

--- Database.py
+++ Database.py
@@ -48,45 +48,73 @@ class Database():
         type(targets)
         self.target = targets
 
-    def split_data(self):
+    # def split_data(self):
+    #     unique, counts = np.unique(db.target, return_counts=True)
+    #     occurrences = dict(zip(unique, counts))
+    #     num_user = self.num_user()
+    #     # numero di utenti dopo i quali si inserisce l'utente nel PG
+    #     skipped_user = round(num_user* self.pn/100)
+    #     print(skipped_user)
+    #     count=0
+    #     template=0
+    #     gallery_data, gallery_target, probe_data,probe_target, train_data, train_target, test_data,test_target = [], [], [], [], [], [], [], []
+    #     for i,val in enumerate(self.target):
+    #         occ = occurrences[val]
+    #         # print("it:",i,"count:",count,"skipped:",skipped_user,"cond:",count<skipped_user,"template:", template,"occ:",occ,"user:",val,)
+    #         if count<skipped_user:
+    #             if occ == 1:
+    #                 gallery=True
+    #             else:
+    #                 # numero gallery template
+    #                 ngt = round(occ/2)
+    #                 if template<ngt:
+    #                     gallery=True
+    #                 else:
+    #                     gallery = False
+    #             template = template+1
+    #             if template == occ:
+    #                 template = 0
+    #                 count = count+1
+    #         else:
+    #             template = template+1
+    #             gallery = False
+    #             if template == occ:
+    #                 template = 0
+    #                 if count == skipped_user:
+    #                     count = 0
+    #         if gallery:
+    #             gallery_data.append(self.data[i])
+    #             gallery_target.append(self.target[i])
+    #         else:
+    #             probe_data.append(self.data[i])
+    #             probe_target.append(self.target[i])
+    #     return gallery_data, gallery_target, probe_data, probe_target
+
+    # 0.7 = 30% degli utenti e' nel test ma non nel train
+    def split_data(self,percTest=30):
+        train_data, train_target, test_data,test_target,gallery_data, gallery_target, probe_data,probe_target = [], [], [], [], [], [], [], []
+        num_user = self.num_user()
+        test_no_train = round(num_user * percTest / 100)
+        print(num_user, test_no_train)
+        count = 0
+        template = 0
         unique, counts = np.unique(db.target, return_counts=True)
         occurrences = dict(zip(unique, counts))
-        num_user = self.num_user()
-        #numero di utenti dopo i quali si inserisce l'utente nel PG
-        skipped_user = round(num_user* self.pn/100)
-        print(skipped_user)
-        count=0
-        template=0
-        gallery_data, gallery_target, probe_data,probe_target = [], [], [], []
-        for i,val in enumerate(self.target):
+        for i, val in enumerate(self.target):
             occ = occurrences[val]
-            #print("it:",i,"count:",count,"skipped:",skipped_user,"cond:",count<skipped_user,"template:", template,"occ:",occ,"user:",val,)
-            if (count<skipped_user):
-                if (occ==1):
-                    gallery=True
-                else:
-                    #numero gallery template
-                    ngt = round(occ/2)
-                    if (template<ngt): gallery=True
-                    else: gallery=False
-                template = template+1
-                if (template==occ):
-                    template=0
-                    count = count+1
+            div = round(occ/2)
+            if template<div and count<num_user - test_no_train:
+                train_data.append(self.data[i])
+                train_target.append(self.target[i])
             else:
-                template = template+1
-                gallery=False
-                if (template == occ):
-                    template = 0
-                    if (count==skipped_user):
-                        count=0
-            if (gallery):
-                gallery_data.append(self.data[i])
-                gallery_target.append(self.target[i])
-            else:
-                probe_data.append(self.data[i])
-                probe_target.append(self.target[i])
-        return gallery_data,gallery_target,probe_data,probe_target
+                test_data.append(self.data[i])
+                test_target.append(self.target[i])
+            template += 1
+            if template == occ:
+                template = 0
+                count += 1
+
+        return train_data, train_target, test_data,test_target,gallery_data, gallery_target, probe_data,probe_target
 
     def num_user(self):
         return len(np.unique(self.target))
@@ -94,8 +122,8 @@ class Database():
     def get_template(self,i):
         return self.data[i]
 
-    #converte array del db con pixel in [0,1] in array con pixel in [0,255]
-    def get_normalized_template(self,i):
+    # converte array del db con pixel in [0,1] in array con pixel in [0,255]
+    def get_normalized_template(self, i):
         norm_image = cv2.normalize(self.get_template(i), None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
         norm_image = norm_image.astype(np.uint8)
         if self.db_index == 1:
@@ -106,7 +134,7 @@ class Database():
         return self.target[i]
 
 if __name__ == '__main__':
-    db = Database()
+    db = Database(0)
     print("Numero utenti: ",len(np.unique(db.target)))
     print(len(db.target))
 
@@ -119,6 +147,10 @@ if __name__ == '__main__':
     #     if cv2.waitKey(1) & 0xFF == ord('q'):
     #         break
 
-    gallery_data,gallery_target,probe_data,probe_target = db.split_data()
-    print("gallery:", len(gallery_data), len(gallery_target), len(np.unique(gallery_target)))
-    print("probe:", len(probe_data), len(probe_target), len(np.unique(probe_target)))
+    # gallery_data,gallery_target,probe_data,probe_target = db.split_data()
+    # print("gallery:", len(gallery_data), len(gallery_target), len(np.unique(gallery_target)))
+    # print("probe:", len(probe_data), len(probe_target), len(np.unique(probe_target)))
+
+    train_data, train_target, test_data,test_target,gallery_data, gallery_target, probe_data,probe_target  = db.split_data()
+    print("train:", len(train_data), len(train_target), len(np.unique(train_target)))
+    print("test:", len(test_data), len(test_target), len(np.unique(test_target)))
