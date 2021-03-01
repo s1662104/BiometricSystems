644087d618d6cb8537f6ce2edc11f491e61f7b72
ile_ <staiti.1662105@studenti.uniroma1.it>
2021-03-01 17:45:33 +0100

    UI pt.4 (aggiunto video capture)

--- Main.py
+++ Main.py
@@ -112,7 +112,27 @@ def back(controller, entryCF, labelError, entryName=None):
     controller.show_frame(StartPage)
 
 def videoCapture():
-    pass
+    cap = cv2.VideoCapture(0)
+
+    while (True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        vis = frame.copy()
+
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        crop = detect_face(gray, vis)
+
+        # Display the resulting frame
+        cv2.imshow('frame', vis)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
 
 def detect_face(img, vis, crop=None):
     detector = dlib.get_frontal_face_detector()
