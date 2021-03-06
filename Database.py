56fde94a8d54ccd14b389ec4a2b71640cf30bcc0
ile_ <staiti.1662105@studenti.uniroma1.it>
2021-03-06 09:21:14 +0100

    Olivetti Names

--- Database.py
+++ Database.py
@@ -2,6 +2,53 @@ import numpy as np
 import tarfile
 import cv2
 import os
+from enum import Enum
+import pandas as pd
+
+class Olivetti_Names(Enum):
+    Xander_Bolton = 0
+    Jameson_Sierra = 1
+    Myron_Kay = 2
+    Liam_Cote = 3
+    Ronnie_Connor = 4
+    Carlton_Stubbs = 5
+    Luis_Richards = 6
+    Carolina_Aldred = 7
+    Esme_Suarez = 8
+    Giulia_Sutherland = 9
+    Eduardo_Villalobos = 10
+    Maxime_Koch = 11
+    Francis_Oconnor = 12
+    Omari_Bellamy = 13
+    Harlan_Chapman = 14
+    Kirk_Meadows = 15
+    Darren_Haynes = 16
+    Jim_Bostock = 17
+    Vladimir_Sumner = 18
+    Keyan_Suarez = 19
+    Curtis_Willis = 20
+    Jae_Watt = 21
+    Merlin_Heaton = 22
+    Matthew_Lloyd = 23
+    Finn_Curry = 24
+    Toby_Cabrera = 25
+    Martin_Dickinson = 26
+    Cobie_Whitfield = 27
+    Elyas_Vo = 28
+    Dominick_Deacon = 29
+    Tobey_Davis = 30
+    Caitlyn_Schneider = 31
+    Rayan_Diaz = 32
+    Phillip_Donaldson = 33
+    Karina_Anthony = 34
+    Stan_Curran = 35
+    Aston_Andrews = 36
+    Junior_Pham = 37
+    Konnor_Buck = 38
+    Martyn_Mays = 39
+
+    def __str__(self):
+        return 'my custom str! {0}'.format(self.value)
 
 class Database():
 
@@ -92,9 +139,25 @@ class Database():
             print("VALORE NON VALIDO!")
         return data, target
 
+    def csv_maker(self):
+        dataset = []
+        for user in np.unique(self.gallery_target):
+            row = []
+            row.append(user)
+            dataset.append(row)
+        print(dataset)
+
+    def show_image(self, img):
+        while(True):
+            cv2.imshow('frame', img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
 if __name__ == '__main__':
     db = Database()
     print("gallery:", len(db.gallery_data), len(db.gallery_target), len(np.unique(db.gallery_target)))
     print("probe PG:", len(db.pg_data), len(db.pg_target), len(np.unique(db.pg_target)))
     print("probe PN:", len(db.pn_data), len(db.pn_target), len(np.unique(db.pn_target)))
+
+    db.csv_maker()
 
