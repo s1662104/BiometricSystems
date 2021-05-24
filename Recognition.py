46aebc91f9342c90cafc8fbf89448b4c106068a5
Ale-yato <mazzucchelli.1644537@studenti.uniroma1.it>
2021-05-24 13:24:17 +0200

    Evaluation Identification

--- Recognition.py
+++ Recognition.py
@@ -360,5 +360,5 @@ if __name__ == '__main__':
     verificationFRR()
     #verificationFAR()
     #verificationROC()
-    evaluationIdentificationAsMultiVer()
+    #evaluationIdentificationAsMultiVer()
     #evaluationIdentification()
