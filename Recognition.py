b8539ba1c9c29d0bab95449462f76bb50186284a
Ale-yato <mazzucchelli.1644537@studenti.uniroma1.it>
2021-06-11 10:24:36 +0200

    Evaluation

--- Recognition.py
+++ Recognition.py
@@ -155,6 +155,8 @@ def verificationFRR():
         if gx < gallery_thresholds[index]:
             fr = fr + 1
 
+    print("TRUE GENUINE:", len(pg_target))
+    print("FALSE REJECTION:", fr)
     print("FRR:", fr/len(pg_target))
 
     return
@@ -183,23 +185,25 @@ def verificationFAR():
             #topMatch returns the best match between pg_hist and the templates associated to the claimed identity in the gallery
             val = topMatch(t, gallery_target, histogram_gallery_data, pn_hist)
             if val >= gallery_thresholds[index_target]:
-                fa = fa + 1
+                fa += 1
             ti += 1
             index_target += 1
 
     #Scenario in which the impostor belongs to the gallery
     for i in range(len(histogram_pg_data)):
         pg_hist = histogram_pg_data[i]
+        index_target = 0
         for t in galley_users:
-            index_target = 0
             if t != pg_target[i]:
                 # topMatch returns the best match between pg_hist and the templates associated to the claimed identity in the gallery
                 val = topMatch(t, gallery_target, histogram_gallery_data, pg_hist)
                 if val >= gallery_thresholds[index_target]:
-                    fa = fa + 1
+                    fa += 1
                 ti += 1
             index_target += 1
 
+    print("TRUE IMPOSTOR:", ti)
+    print("FALSE ACCEPTANCE:", fa)
     print("FAR:", fa / ti)
 
     return
@@ -306,7 +310,6 @@ def evaluationIdentificationAsMultiVer():
     print("FRR:", FRR, countTG)
     print("FAR:", FAR, countTI)
 
-#def delegatesMatch(data, target, gallery_target, gallery_data, cf_list, users, gallery_thresholds, histogram_gallery_data):
 def delegatesMatch(hist_data, target, gallery_target, cf_list, users, gallery_thresholds, histogram_gallery_data):
     countTG = 0
     countTI = 0
@@ -369,12 +372,12 @@ def evaluationVerification():
     # print(similarity_matrix)
 
     similarity_matrix = np.load("similarity_matrix.npy")
-    print(similarity_matrix)
 
     tg = len(pg_target)
     ti = len(pg_target)*(len(galley_identities)-1) + len(pn_target)*len(galley_identities)
     print("TG:", tg)
     print("TI:", ti)
+    print()
 
     frr = []
     far = []
@@ -382,7 +385,7 @@ def evaluationVerification():
     grr = []
     thresholds = []
 
-    for threshold in np.arange(0.0, 1.0, 0.01):
+    for threshold in np.arange(0.0, 1.1, 0.01):
         # Fix the threshold
         th = np.round(threshold, 2)
         # Reset all variables
@@ -427,6 +430,7 @@ def evaluationVerification():
     print("FAR:", far)
     print("FRR:", frr)
     print("GRR:", grr)
+    print()
 
     eer_1 = np.array(far)[np.nanargmin(np.absolute((np.array(frr) - np.array(far))))]
     eer_2 = np.array(frr)[np.nanargmin(np.absolute((np.array(frr) - np.array(far))))]
@@ -435,6 +439,8 @@ def evaluationVerification():
 
     eer_threshold = np.array(thresholds)[np.nanargmin(np.absolute((np.array(frr) - np.array(far))))]
     print("EER Threshold:", eer_threshold)
+    print()
+    print("Thresholds:", thresholds)
 
     plt.plot(far, gar)
     plt.plot([0, 1], [0, 1], 'k--')
@@ -447,14 +453,95 @@ def evaluationVerification():
 
     return
 
+def verificationAdaptive():
+    pg_target = np.load("npy_db/pg_target.npy")
+    pn_target = np.load("npy_db/pn_target.npy")
+    gallery_target = np.load("npy_db/gallery_target.npy")
+    gallery_thresholds = np.load("npy_db/gallery_thresholds.npy")
+    histogram_pg_data = np.load("npy_db/histogram_pg_data.npy")
+    galley_users = list(dict.fromkeys(gallery_target))
+    similarity_matrix = np.load("similarity_matrix.npy")
+
+    tg = len(pg_target)
+    ti = len(pg_target) * (len(galley_users) - 1) + len(pn_target) * len(galley_users)
+
+    for decrement in np.arange(0.00, 0.11, 0.01):
+        ga = 0
+        fa = 0
+        fr = 0
+        gr = 0
+        for x in range(similarity_matrix.shape[0]):
+            if x < len(histogram_pg_data):
+                real_identity = pg_target[x]
+            else:
+                real_identity = pn_target[x - len(histogram_pg_data)]
+            for y in range(similarity_matrix.shape[1]):
+                claim_identity = galley_users[y]
+                similarity = similarity_matrix[x][y]
+                if similarity >= (gallery_thresholds[y]-decrement):
+                    if real_identity == claim_identity:
+                        ga += 1
+                    else:
+                        fa += 1
+                else:
+                    if real_identity == claim_identity:
+                        fr += 1
+                    else:
+                        gr += 1
+        print("Decrementando i threshold adattivi di", decrement,"il FRR è uguale  a:", fr/tg)
+        print("Decrementando i threshold adattivi di", decrement,"il FAR è uguale  a:", fa/ti)
+        print()
+
+    return
+
 if __name__ == '__main__':
-    verificationFRR()
+    #verificationFRR()
     #verificationFAR()
     #verificationROC()
     #evaluationIdentificationAsMultiVer()
+    #verificationAdaptive()
     #evaluationVerification()
-    #far = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.9997198879551821, 0.9997198879551821, 0.9991596638655462, 0.9984593837535014, 0.9974789915966387, 0.9957983193277311, 0.9928571428571429, 0.9882352941176471, 0.9831932773109243, 0.9746498599439776, 0.9644257703081233, 0.9511204481792717, 0.9329131652661065, 0.9119047619047619, 0.8813725490196078, 0.846078431372549, 0.8081232492997199, 0.7675070028011205, 0.7177871148459384, 0.6668067226890756, 0.6095238095238096, 0.5497198879551821, 0.49089635854341734, 0.426750700280112, 0.3680672268907563, 0.315266106442577, 0.26554621848739496, 0.22198879551820727, 0.18207282913165265, 0.14523809523809525, 0.11708683473389356, 0.09243697478991597, 0.07226890756302522, 0.05742296918767507, 0.044677871148459385, 0.033473389355742296, 0.025070028011204483, 0.018067226890756304, 0.012324929971988795, 0.008963585434173669, 0.0056022408963585435, 0.0036414565826330533, 0.0021008403361344537, 0.0016806722689075631, 0.0014005602240896359, 0.0004201680672268908, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    #frr = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.007142857142857143, 0.007142857142857143, 0.007142857142857143, 0.007142857142857143, 0.007142857142857143, 0.02142857142857143, 0.02142857142857143, 0.02142857142857143, 0.02142857142857143, 0.02857142857142857, 0.02857142857142857, 0.04285714285714286, 0.05, 0.06428571428571428, 0.07142857142857142, 0.09285714285714286, 0.12857142857142856, 0.15, 0.17142857142857143, 0.17857142857142858, 0.20714285714285716, 0.24285714285714285, 0.2785714285714286, 0.3, 0.34285714285714286, 0.38571428571428573, 0.45, 0.5357142857142857, 0.5785714285714286, 0.6285714285714286, 0.6714285714285714, 0.75, 0.8, 0.8428571428571429, 0.8785714285714286, 0.9214285714285714, 0.9428571428571428, 0.9571428571428572, 0.9857142857142858, 0.9857142857142858, 0.9928571428571429, 0.9928571428571429, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-    #index = far.index(0.018067226890756304)
-    #print("FRR:", frr[index])
-    #print("FAR:", 0.018067226890756304)
+
+    far = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+          1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.9997198879551821, 0.9997198879551821, 0.9991596638655462,
+          0.9984593837535014, 0.9974789915966387, 0.9957983193277311, 0.9928571428571429, 0.9882352941176471,
+          0.9831932773109243, 0.9746498599439776, 0.9644257703081233, 0.9511204481792717, 0.9329131652661065,
+          0.9119047619047619, 0.8813725490196078, 0.846078431372549, 0.8081232492997199, 0.7675070028011205,
+          0.7177871148459384, 0.6668067226890756, 0.6095238095238096, 0.5497198879551821, 0.49089635854341734,
+          0.426750700280112, 0.3680672268907563, 0.315266106442577, 0.26554621848739496, 0.22198879551820727,
+          0.18207282913165265, 0.14523809523809525, 0.11708683473389356, 0.09243697478991597, 0.07226890756302522,
+          0.05742296918767507, 0.044677871148459385, 0.033473389355742296, 0.025070028011204483, 0.018067226890756304,
+          0.012324929971988795, 0.008963585434173669, 0.0056022408963585435, 0.0036414565826330533,
+          0.0021008403361344537, 0.0016806722689075631, 0.0014005602240896359, 0.0004201680672268908, 0.0, 0.0, 0.0,
+          0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+          0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+    frr = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+          0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+          0.0, 0.0, 0.0, 0.0, 0.0, 0.007142857142857143, 0.007142857142857143, 0.007142857142857143,
+          0.007142857142857143, 0.007142857142857143, 0.02142857142857143, 0.02142857142857143, 0.02142857142857143,
+          0.02142857142857143, 0.02857142857142857, 0.02857142857142857, 0.04285714285714286, 0.05, 0.06428571428571428,
+          0.07142857142857142, 0.09285714285714286, 0.12857142857142856, 0.15, 0.17142857142857143, 0.17857142857142858,
+          0.20714285714285716, 0.24285714285714285, 0.2785714285714286, 0.3, 0.34285714285714286, 0.38571428571428573,
+          0.45, 0.5357142857142857, 0.5785714285714286, 0.6285714285714286, 0.6714285714285714, 0.75, 0.8,
+          0.8428571428571429, 0.8785714285714286, 0.9214285714285714, 0.9428571428571428, 0.9571428571428572,
+          0.9857142857142858, 0.9857142857142858, 0.9928571428571429, 0.9928571428571429, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+          1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+
+    thresholds = [0.0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16,
+                 0.17, 0.18, 0.19, 0.2, 0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.3, 0.31, 0.32, 0.33,
+                 0.34, 0.35, 0.36, 0.37, 0.38, 0.39, 0.4, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.5,
+                 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.6, 0.61, 0.62, 0.63, 0.64, 0.65, 0.66, 0.67,
+                 0.68, 0.69, 0.7, 0.71, 0.72, 0.73, 0.74, 0.75, 0.76, 0.77, 0.78, 0.79, 0.8, 0.81, 0.82, 0.83, 0.84,
+                 0.85, 0.86, 0.87, 0.88, 0.89, 0.9, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99, 1.0, 1.01,
+                 1.02, 1.03, 1.04, 1.05, 1.06, 1.07, 1.08, 1.09]
+
+    # index = thresholds.index(0.62)
+    # print("Valori ottenuti utilizzando il eer_threshold = 0.62")
+    # print("FRR:", frr[index])
+    # print("FAR:", far[index])
+    index = far.index(0.0036414565826330533)
+    print("Valori ottenuti utilizzando un threshold =", thresholds[index], "che ci dia un far simile a quello ottenuto con i threshold adattivi")
+    print("FRR:", frr[index])
+    print("FAR:", far[index])
+
